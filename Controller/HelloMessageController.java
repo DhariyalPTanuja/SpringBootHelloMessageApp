152a7912742f0c342b771b4059944a084bb61ffd
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-28 12:25:26 +0530

    Pass name as query parameter

--- Controller/HelloMessageController.java
+++ Controller/HelloMessageController.java
@@ -1,18 +1,16 @@
 package com.example.hellomessagingapp.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloMessageController {
-    @RequestMapping("/welcome")
-    public String sayHello() {
-        return "welcome  to Hello Messaging App";
-    }
 
-    @RequestMapping("/msg")
-    public String sayHelloMsg() {
-        return "Hello From BridgeLabz !!!! ";
+    @RequestMapping("/query")
+    //http://localhost:8080/query?name=Mark
+    public String sayHelloMsg(@RequestParam(value = "name") String name) {
+        return "Hello " + name + "! ";
     }
 
 }
