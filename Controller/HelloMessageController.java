baba0bacba434f77b91ea864d350d940a910ae11
DhariyalPTanuja <dhariyal.ptanuja@gmail.com>
2022-11-28 13:18:19 +0530

    using Post Request and Pass name in the Body

--- Controller/HelloMessageController.java
+++ Controller/HelloMessageController.java
@@ -1,17 +1,13 @@
 package com.example.hellomessagingapp.Controller;
 
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloMessageController {
 
-    @RequestMapping("/param/{name}")
-   //http://localhost:8080/param/mark
-    public String sayHelloMsg(@PathVariable String name) {
-        return "Hello " + name + " Welcome to Bridgelabz!! ";
+    @PostMapping("/post")
+    public String sayHelloMsg(@RequestBody String firstName, String lastName){
+        return "Hello " + firstName + " " + lastName + " Welcome to Bridgelabz!!";
     }
 
 }
